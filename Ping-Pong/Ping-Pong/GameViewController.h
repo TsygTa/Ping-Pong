5eb0fa0665d5b283c23796b93dd9ec619cb55273
TsygTa <670724@gmail.com>
2019-04-23 14:43:10 +0300

    Lesson-7

---
+++ Ping-Pong/Ping-Pong/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ping-Pong
+//
+//  Created by Tatiana Tsygankova on 23.04.2019.
+//  Copyright © 2019 Tatiana Tsygankova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+
+@end
+
